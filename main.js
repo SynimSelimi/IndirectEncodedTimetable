0b3280f677f6a2ace3f354ada2945d9662078ac2
SynimSelimi <26981059+SynimSelimi@users.noreply.github.com>
2021-12-26 00:00:49 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,13 +6,20 @@
 const numberOfExams = 8;
 const exams = Array.from({length: numberOfExams}, (_, i) => i + 1)
 const numberOfSlots = 16;
-const examsWithSameStudents = [[1, 2], [4, 5, 8], [6, 4, 1]];
+const slotsPerDay = 4;
 const sameStudentsExam = {};
-examsWithSameStudents.forEach((exams) => {
-    exams.forEach((exam) => sameStudentsExam[exam] = exams);
-});
-
 let encoding = [4, 5, 13, 1, 1, 4, 13, 2];
+
+// CLASHES
+const examsWithSameStudents = [[1, 2], [4, 5, 8], [6, 4, 1]];
+examsWithSameStudents.forEach((exams) => {
+    exams.forEach((exam) => {
+        if (typeof sameStudentsExam[exam] !== 'object')
+            sameStudentsExam[exam] = exams;
+        else
+            sameStudentsExam[exam] = [...new Set([...sameStudentsExam[exam], ...exams])];
+    });
+});
 
 // FUNCTIONS
 const newEncoding = () => {
@@ -27,12 +34,37 @@ const mutateEncoding = (encoding) => {
     return _encoding;
 };
 
-const newSolution = (encoding) => {
-    encoding.map((assignment) => {
-      return assignment;
-    });
+const noClasesh = (exam, scheduledExams) => {
+    const clashes = sameStudentsExam[exam];
+    if (!clashes) return true;
+
+    const assignments = scheduledExams.flatMap((sched) => sameStudentsExam[sched.split('E')[1]]);
+    return !clashes.some((clash) => assignments.includes(clash));
 };
 
-encoding = newEncoding();
+const newSolution = (encoding) => {
+    const solution = Array.from(Array(numberOfSlots), () => []);
+    encoding.forEach((assignment, index) => {
+        const exam = index + 1;
+        var countAssignment = 0;
+        
+        for (var i = 0; i < numberOfSlots; i++ ) {
+            const hasNoClash = noClasesh(exam, solution[i]);
+            if (countAssignment === assignment) {
+                solution[i].push(`E${exam}`);
+                break;
+            } else if (hasNoClash) {
+                countAssignment++;
+            } else {
+                countAssignment = 0;
+            }
+        }
+    });
+    return solution;
+};
 
-console.log(encoding, mutateEncoding(encoding), mutateEncoding(encoding), mutateEncoding(encoding));
+// MAIN
+// encoding = newEncoding();
+// console.log(encoding, mutateEncoding(encoding), mutateEncoding(encoding), mutateEncoding(encoding));
+const solution = newSolution(encoding);
+console.log(solution);
